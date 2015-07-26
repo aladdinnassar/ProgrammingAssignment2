f2061fb7b685bfb960c68dbd67a7b5798c982e1b
Aladdin Nassar <aladdin.nassar@gmail.com>
2015-07-26 00:01:00 -0700

    Checking in Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to Calculate and Cache Matrix Inversion
 
-## Write a short comment describing this function
+## Usage:
+## m <- matrix(c(1,0,0,2),nrow=2, ncol=2)
+## ma <- makeCacheMatrix(m)
+## mb <- cacheSolve(ma)         // First Use
+## mb <- cacheSolve(ma)         // Cached Use
 
+## Need the MASS Package to use the ginv() function to invert matrices
+library(MASS)
+
+## Function anologous to a Class in Object-Oriented Programming Languages
+## Defines the Set and Get Inverse Functions for a Matrix
+## Use the Matrix Cache when called after the first time
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(ginv) m <<- ginv
+    getinv <- function() m
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve = Matrix Inversion with Caching
+## Returns the cached inverted matrix if available from previous calls
+## It first calls the getinv function from the above function that return the matrix m
+## if the matrix m is not null, it returns the cached value
+## Otherwise, it calls teh setinv function to get the matrix' inverse which it caches and returns
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- ginv(data, ...)
+    x$setinv(m)
+    m
 }
+
+
+
+
+
+
